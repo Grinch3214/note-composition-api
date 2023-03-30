ca1c0812fb423b8703cff614bce892141ac7aee7
Max <scorpiomax90@gmail.com>
2023-03-30 13:15:49 +0300

    use ref and added methods

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -5,25 +5,31 @@
 			<div class="field">
 				<div class="control">
 					<textarea
+						v-model="newNote"
 						class="textarea"
 						placeholder="Add a new note"
+						ref="newNoteRef"
 					/>
 				</div>
 			</div>
 
 			<div class="field is-grouped is-grouped-right">
 				<div class="control">
-					<button class="button is-link has-background-success">
+					<button
+						@click="addNote"
+						:disabled="!newNote"
+						class="button is-link has-background-success"
+					>
 						Add New Note
 					</button>
 				</div>
 			</div>
 		</div>
 
-		<div class="card mb-4" v-for="i in 3" :key="i">
+		<div class="card mb-4" v-for="note in notes" :key="note.id">
 			<div class="card-content">
 				<div class="content">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia nemo facilis consequuntur aliquid itaque ea dolor non ipsum quo dignissimos nam, perferendis deleniti dolorem neque temporibus animi velit iste!
+					{{ note.content }}
 				</div>
 			</div>
 			<footer class="card-footer">
@@ -34,3 +40,44 @@
 	</div>
 	
 </template>
+
+<script setup>
+	// imports
+	import { ref } from 'vue'
+
+	// new note
+	const newNote = ref('')
+	const newNoteRef = ref(null)
+
+	// notes
+	const notes = ref([
+		{
+			id: 1,
+			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia nemo facilis consequuntur aliquid itaque ea dolor non ipsum quo dignissimos nam, perferendis deleniti dolorem neque temporibus animi velit iste!'
+		},
+		{
+			id: 2,
+			content: 'Molestias officia nemo facilis consequuntur aliquid itaque ea dolor non ipsum quo dignissimos nam, perferendis deleniti dolorem neque temporibus animi velit iste!'
+		},
+		{
+			id: 3,
+			content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia nemo facilis consequuntur aliquid itaque ea '
+		}
+	])
+
+	const addNote = () => {
+		let currentDate = new Date().getTime()
+		let id = currentDate
+
+		let note = {
+			id,
+			content: newNote.value
+		}
+
+		notes.value.unshift(note)
+
+		newNote.value = ''
+		newNoteRef.value.focus()
+
+	}
+</script>
