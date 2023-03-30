59d9d357e23c5eeabe8442a4926ae7207f9d5f6c
Max <scorpiomax90@gmail.com>
2023-03-30 09:23:59 +0300

    added bulma css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-	<RouterLink to="/">Notes</RouterLink> |
-	<RouterLink to="/stats">Stats</RouterLink>
+	<RouterLink class="button" to="/">Notes</RouterLink> |
+	<RouterLink class="button" to="/stats">Stats</RouterLink>
 
 	<RouterView />
 </template>
+
+
+<style>
+	@import 'bulma/css/bulma.min.css';
+</style>
